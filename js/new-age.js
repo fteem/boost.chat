779bac6b1d9098312dd32c7c87176036f42ac706
Ilija Eftimov <ileeftimov@gmail.com>
2018-03-26 12:05:45 +0200

    Disable navbar scripts

--- js/new-age.js
+++ js/new-age.js
@@ -16,27 +16,27 @@
   });
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+  //$('.js-scroll-trigger').click(function() {
+    //$('.navbar-collapse').collapse('hide');
+  //});
 
   // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 54
-  });
+  //$('body').scrollspy({
+    //target: '#mainNav',
+    //offset: 54
+  //});
 
   // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  };
+  //var navbarCollapse = function() {
+    //if ($("#mainNav").offset().top > 100) {
+      //$("#mainNav").addClass("navbar-shrink");
+    //} else {
+      //$("#mainNav").removeClass("navbar-shrink");
+    //}
+  //};
   // Collapse now if page is not at top
-  navbarCollapse();
+  //navbarCollapse();
   // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+  //$(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
